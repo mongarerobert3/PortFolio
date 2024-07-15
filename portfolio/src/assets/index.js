758bdd15b6e3e0556e63148f1629533ed909f4a9
mongarerobert3 <mongarerobert3@gmail.com>
2024-07-15 23:44:50 +0300

    updates

---
+++ portfolio/src/assets/index.js
@@ -0,0 +1,68 @@
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import dms from "./dms.png";
+
+import aws from "./tech/aws.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import azure from "./tech/azure.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import Google_Cloud from "./tech/Google_Cloud.png";
+import Kubernetes from "./tech/Kubernetes.png";
+import mysql from "./tech/mysql.png";
+import oracle from "./tech/oracle.png";
+import terraform from "./tech/terraform.png";
+import python from "./tech/python.png";
+
+import meta from "./company/meta.png";
+import postbank from "./company/postbank.jfif";
+import pixels from "./company/pixels.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.jpg";
+
+export {
+  backend,
+  creator,
+  dms,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  docker,
+  figma,
+  python,
+  terraform,
+  aws,
+  oracle,
+  mysql,
+  azure,
+  Kubernetes,
+  Google_Cloud,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  meta,
+  postbank,
+  pixels,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ portfolio/src/constants/index.js
@@ -0,0 +1,266 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+
+  figma,
+  docker,
+  meta,
+  postbank,
+  pixels,
+  carrent,
+  jobit,
+  python,
+  tripguide,
+  terraform,
+  aws,
+  dms,
+  oracle,
+  mysql,
+  azure,
+  Kubernetes,
+  Google_Cloud,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+  {
+    id: "resume",
+    title: "Resume",
+  },
+];
+
+const services = [
+  {
+    title: "DevOps Engineer",
+    icon: web,
+  },
+  {
+    title: "Software Engineer",
+    icon: mobile,
+  },
+  {
+    title: "Automation Engineer",
+    icon: backend,
+  },
+  {
+    title: "Web Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "terraform",
+    icon: terraform,
+  },
+  {
+    name: "aws",
+    icon: aws,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "Mysql",
+    icon: mysql,
+  },
+  {
+    name: "Azure",
+    icon: azure,
+  },
+  {
+    name: "Kubernetes",
+    icon: Kubernetes,
+  },
+  {
+    name: "Google_Cloud",
+    icon: Google_Cloud,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "python",
+    icon: python,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "oracle",
+    icon: oracle,
+  },
+];
+
+const experiences = [
+  {
+    title: "IT intern ",
+    company_name: "Postbank Kenya",
+    icon: postbank,
+    iconBg: "#383E56",
+    date: "March 2024 - July 2024",
+    points: [
+      "Designed and implemented a robust reporting system capable of dynamically generating tables from JSON inputs. This system supports exporting data to custom PDF and Excel formats, along with automated scheduling for email reports.",
+      "Contributed significantly to the restoration of a helpdesk software system, ensuring its functionality and reliability.",
+    ],
+  },
+  {
+    title: "Developer",
+    company_name: "254 Pixels Marketing",
+    icon: pixels,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Dec 2023",
+    points: [
+      "Contributed to the development of educational systems using various CMS technologies, aimed at creating customized solutions for multiple agencies. Implemented user-friendly interfaces and integrated functionalities tailored to specific educational needs.",
+      "Designed and implemented WordPress-based e-commerce platforms for diverse clients, focusing on optimizing performance and scalability. Integrated payment gateways, customized themes, and plugins to deliver seamless shopping experiences.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "freelance",
+    icon: web,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Develop and maintain web applications for diverse clients using MERN Stack, Wordpress  and other related tech.",
+    ],
+  },
+  
+];
+
+
+const projects = [
+  {
+    name: "Tours n Travel",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "Reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Tech Spaces",
+    description:
+      "Web application that connects developers with tech spaces such as co-working spaces, meetups, hackathons, and conferences in their area. The application allows developers to search for tech spaces by location, technology, and event type, and also provides a chat feature for developers to communicate with each other.",
+    tags: [
+      {
+        name: "Angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Node",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Mongodb",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Dairy Management system",
+    description:
+      "The Dairy Management System project streamlines farm operations through automated data collection, analysis, and reporting.",
+    tags: [
+      {
+        name: "python",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "django",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mysql",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: dms,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Fruity",
+    description:
+      "Fruity is a modern and user-friendly mobile application designed to make buying and selling fruits easier than ever before. With Fruity, users can browse through a wide variety of fresh and delicious fruits, place orders, and even start their own fruit-selling business hassle-free.",
+    tags: [
+      {
+        name: "React-native",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Mongodb",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, projects };
